475ac69146472d92a37aa8978a79519439a9fdaf
steve <stevekitkat@gmail.com>
2025-07-31 13:25:05 +0700

    Updates the librarys and gradle

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -4,9 +4,9 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.android.application)
     alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kotlin.compose)
 }
 
 kotlin {
