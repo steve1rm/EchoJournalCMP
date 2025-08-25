59819ca8d1866bf4666ff6ceb04bfa17b8e8d4d8
steve <stevekitkat@gmail.com>
2025-08-25 23:28:52 +0100

    3.4 dropdown completed

--- composeApp/src/commonMain/kotlin/org/example/echojournalcmp/core/presentation/designsystem/dropdowns/SelectableDropdownOptionsMenu.kt
+++ composeApp/src/commonMain/kotlin/org/example/echojournalcmp/core/presentation/designsystem/dropdowns/SelectableDropdownOptionsMenu.kt
@@ -1,25 +1,37 @@
 package org.example.echojournalcmp.core.presentation.designsystem.dropdowns
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.key.Key.Companion.R
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
 import echojournalcmp.composeapp.generated.resources.Res
+import echojournalcmp.composeapp.generated.resources.add
+import echojournalcmp.composeapp.generated.resources.check
 import echojournalcmp.composeapp.generated.resources.hashtag
 import org.example.echojournalcmp.core.presentation.designsystem.dropdowns.Selectable.Companion.asUnselectedItems
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.resources.vectorResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -29,7 +41,8 @@ fun <T> SelectableDropdownOptionsMenu(
     items: List<Selectable<T>>,
     onDismiss: () -> Unit,
     key: (T) -> Any,
-    onItemClick: () -> Unit,
+    onItemClick: (selectable: Selectable<T>) -> Unit,
+    itemDisplayText: (item: T) -> String,
     leadingIcon: (@Composable () -> Unit)? = null,
     dropDownOffset: IntOffset = IntOffset.Zero,
     maxDropDownHeight: Dp = Dp.Unspecified,
@@ -56,15 +69,80 @@ fun <T> SelectableDropdownOptionsMenu(
             LazyColumn(
                 modifier = Modifier
                     .animateContentSize()
-                    .padding(6.dp)
+                    .padding(6.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
             ) {
                 items(
                     items = items,
                     key = { selectable ->
                         key(selectable.item)
                     }
-                ) {
+                ) {selectable ->
+                    Row(
+                        modifier = Modifier
+                            .animateItem()
+                            .fillMaxWidth()
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(
+                                color = if(selectable.selected) {
+                                    MaterialTheme.colorScheme.surfaceTint.copy(alpha = 0.0f)
+                                }
+                                else {
+                                    MaterialTheme.colorScheme.surface
+                                }
+                            )
+                            .clickable {
+                                onItemClick(selectable)
+                            }
+                            .padding(8.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        leadingIcon?.invoke()
 
+                        Text(
+                            modifier = Modifier.weight(1f),
+                            text = itemDisplayText(selectable.item),
+                        )
+
+                        if(selectable.selected) {
+                            Icon(
+                                imageVector = vectorResource(Res.drawable.check),
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+                        }
+                    }
+                }
+
+                if(dropDownExtra != null && dropDownExtra.text.isNotEmpty()) {
+                    item(key = true) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(8.dp))
+                                .background(MaterialTheme.colorScheme.surface)
+                                .clickable {
+                                    dropDownExtra.onClick
+                                },
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .size(18.dp),
+                                imageVector = vectorResource(Res.drawable.add),
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+
+                            Text(
+                                modifier = Modifier.weight(1f),
+                                text = "Create entry",
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -76,9 +154,21 @@ fun <T> SelectableDropdownOptionsMenu(
 fun SelectableDropdownOptionsMenuPreview() {
     SelectableDropdownOptionsMenu(
         items = listOf(
-            Selectable("Option 1", true),
-            Selectable("Option 2", false)
-        ).asUnselectedItems(),
+            Selectable(
+                item = "Hello world 1",
+                selected = false
+            ),
+            Selectable(
+                item = "Hello world 2",
+                selected = true
+            ),
+            Selectable(
+                item = "Hello world 3",
+                selected = true
+            ),
+        ),
+        key = { it },
+        itemDisplayText = { it },
         onDismiss = {},
         onItemClick = {},
         leadingIcon = {
@@ -88,9 +178,5 @@ fun SelectableDropdownOptionsMenuPreview() {
             )
         },
         maxDropDownHeight = 500.dp,
-        dropDownExtra = SelectableOptionExtra(
-            "All topics",
-            onClick = {}
-        )
     )
 }
