3139641dfcb24b84393b3df7b3384aecdc1412a9
steve <stevekitkat@gmail.com>
2025-09-01 23:57:34 +0100

    4.2 ObserveAsEvents completed

---
+++ composeApp/src/commonMain/kotlin/org/example/echojournalcmp/core/presentation/util/ObserveAsEvents.kt
@@ -0,0 +1,29 @@
+package org.example.echojournalcmp.core.presentation.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+
+@Composable
+fun <T> ObserveAsEvents(
+    flow: Flow<T>,
+    key1: Any? = null,
+    key2: Any? = null,
+    onEvent: (event: T) -> Unit
+) {
+    // Ignore all events when the app is in the background
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+
+    LaunchedEffect(lifecycle, key1, key2) {
+        lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            withContext(Dispatchers.Main.immediate) {
+                flow.collect(onEvent)
+            }
+        }
+    }
+}
