0e488b9bca6e22d508da2eb252650a9803ce6682
steve <stevekitkat@gmail.com>
2025-09-15 09:02:40 +0100

    5.11 quick record

--- composeApp/src/commonMain/kotlin/org/example/echojournalcmp/echos/presentation/components/EchoRecordingSheet.kt
+++ composeApp/src/commonMain/kotlin/org/example/echojournalcmp/echos/presentation/components/EchoRecordingSheet.kt
@@ -33,7 +33,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import echojournalcmp.composeapp.generated.resources.Res
 import echojournalcmp.composeapp.generated.resources.cancel
@@ -45,8 +47,11 @@ import echojournalcmp.composeapp.generated.resources.paused
 import echojournalcmp.composeapp.generated.resources.recording_your_memories
 import org.example.echojournalcmp.core.presentation.designsystem.theme.EchoJournalCMPTheme
 import org.example.echojournalcmp.core.presentation.designsystem.theme.buttonGradient
+import org.example.echojournalcmp.core.presentation.designsystem.theme.buttonGradientPressed
 import org.example.echojournalcmp.core.presentation.designsystem.theme.primary90
 import org.example.echojournalcmp.core.presentation.designsystem.theme.primary95
+import org.example.echojournalcmp.echos.presentation.echos.model.BubbleFloatingActionButtonColors
+import org.example.echojournalcmp.echos.presentation.echos.model.rememberBubbleFloatingActionButtonColors
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.resources.vectorResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -174,7 +179,9 @@ fun SheetContent(
                     )
                     .padding(16.dp)
                     .background(
-                        brush = MaterialTheme.colorScheme.buttonGradient,
+                        brush = if(isPressed)
+                            MaterialTheme.colorScheme.buttonGradientPressed
+                        else MaterialTheme.colorScheme.buttonGradient,
                         shape = CircleShape
                     )
                     .padding(16.dp)
@@ -218,6 +225,62 @@ fun SheetContent(
                 )
             }
         }
+    }
+}
+
+@Composable
+fun EchoBubbleFloatingActionButton(
+    showBubble: Boolean,
+    onClick: () -> Unit,
+    icon: @Composable () -> Unit,
+    primaryButtonSize: Dp = 56.dp,
+    colors: BubbleFloatingActionButtonColors = rememberBubbleFloatingActionButtonColors(),
+    modifier: Modifier = Modifier
+) {
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    Box(
+        modifier = modifier
+            .background(
+                brush = if(showBubble) {
+                    colors.outerCircle
+                }
+                else {
+                    SolidColor(Color.Transparent)
+                },
+                shape = CircleShape
+            )
+            .padding(10.dp)
+            .background(
+                brush = if(isPressed) {
+                    colors.primaryPressed
+                }
+                else {
+                    SolidColor(Color.Transparent)
+                },
+                shape = CircleShape
+            )
+            .padding(16.dp)
+            .background(
+                brush = if(isPressed)
+                    MaterialTheme.colorScheme.buttonGradientPressed
+                else MaterialTheme.colorScheme.buttonGradient,
+                shape = CircleShape
+            )
+            .size(primaryButtonSize)
+            .clip(CircleShape)
+            .clickable(
+                interactionSource = interactionSource,
+                indication = LocalIndication.current,
+                onClick = onClick
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        icon()
     }
 }
 
